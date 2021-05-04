078f528d718d690b52668e2ec74d6dc23d5942e0
Yukio Tanaka <news7vip@gmail.com>
2021-05-04 15:31:57 +0900

    Add executer for using scheme functions from Rust

--- src/mini-scheme/src/lib.rs
+++ src/mini-scheme/src/lib.rs
@@ -3,8 +3,10 @@
 use std::sync::Arc;
 
 use either::Either;
-use interpreter::{Env, Interpreter};
+use interpreter::{Env, ExecutionResult, Interpreter};
 use parser::Parser;
+
+use crate::ast::{Expr, SExpr, TopLevel};
 
 mod ast;
 mod interpreter;
@@ -94,5 +96,102 @@ impl Repl {
 impl Default for Repl {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+pub struct Executer {
+    interpreter: Interpreter,
+}
+
+impl Executer {
+    pub fn new(lines: String) -> Result<Self, String> {
+        let lexed = lexer::lex(&lines);
+        let parser = Parser::new(lexed);
+        let interpreter = Interpreter::new(Arc::new(|x| println!("{}", x)));
+        while let Some(pp) = parser.parse_toplevel() {
+            interpreter.execute_toplevel(pp);
+        }
+        let executor = Self { interpreter };
+        Ok(executor)
+    }
+
+    pub fn execute_func(
+        &self,
+        name: &str,
+        args: Vec<Box<dyn AsArg>>,
+    ) -> Result<Either<ExecutionResult, Vec<ExecutionResult>>, String> {
+        let r = self
+            .interpreter
+            .execute_toplevel(TopLevel::Expr(Expr::Apply(
+                Box::new(Expr::Id(name.to_string())),
+                args.iter().map(|x| x.to_const_expr()).collect(),
+            )));
+        match r {
+            Either::Left(Ok(r)) => Ok(Either::Left(r)),
+            Either::Left(Err(e)) => Err(e),
+            Either::Right(r) => r
+                .into_iter()
+                .collect::<Result<Vec<_>, _>>()
+                .map(Either::Right),
+        }
+    }
+}
+
+pub trait AsArg {
+    fn to_const_expr(&self) -> Expr;
+}
+
+impl AsArg for &str {
+    fn to_const_expr(&self) -> Expr {
+        Expr::Const((*self).into())
+    }
+}
+
+impl AsArg for i32 {
+    fn to_const_expr(&self) -> Expr {
+        Expr::Const((*self as i64).into())
+    }
+}
+
+impl AsArg for i64 {
+    fn to_const_expr(&self) -> Expr {
+        Expr::Const((*self).into())
+    }
+}
+
+impl AsArg for bool {
+    fn to_const_expr(&self) -> Expr {
+        Expr::Const((*self).into())
+    }
+}
+
+impl AsArg for Vec<Box<dyn AsArg>> {
+    fn to_const_expr(&self) -> Expr {
+        let exprs = self
+            .iter()
+            .map(|x| x.to_const_expr())
+            .map(|x| match x {
+                Expr::Const(c) => SExpr::Const(c),
+                Expr::Quote(SExpr::SExprs(s, _)) => SExpr::SExprs(s, None),
+                _ => panic!(),
+            })
+            .collect::<Vec<_>>();
+        Expr::Quote(SExpr::SExprs(exprs, None))
+    }
+}
+
+#[test]
+fn test_list_func() {
+    let exec = Executer::new("(define (x y) y)".to_string()).unwrap();
+    let inner_inner: Vec<Box<dyn AsArg>> = vec![Box::new(23)];
+    let list_inner: Vec<Box<dyn AsArg>> = vec![
+        Box::new("30"),
+        Box::new(34),
+        Box::new(32),
+        Box::new(inner_inner),
+    ];
+    let r = exec.execute_func("x", vec![Box::new(list_inner)]).unwrap();
+    if let Either::Left(l) = r {
+        assert_eq!(r#"("30" 34 32 (23))"#, l.to_string());
     }
 }
